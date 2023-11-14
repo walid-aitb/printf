2afd5697b729ae043452dce801104a45cf922708
walid-aitb <walidaitboussmane@gmail.com>
2023-11-14 05:47:15 -0800

    Added prntf_binary.c

---
+++ prntf_binary.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * prntf_binary - converts decimal to binary
+ * @n: argument
+ * Return: count
+ */
+
+int print_binary(unsigned int n)
+
+{
+ int count = 0; 
+ int binary[64]; /* Assuming a 64 bit system */
+ int i = 0;
+ int j;
+
+if (n == 0)
+{
+_putchar('0');
+_putchar('\n');
+return (1);
+}
+
+while (n > 0)
+{
+binary[i] = n % 2;
+n /= 2;
+i++;
+}
+
+for (j = i - 1; j >= 0; j--)
+{
+_putchar('0' + binary[j]);
+count++;
+}
+
+_putchar('\n');
+
+return count;
+}
